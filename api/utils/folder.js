58dc8d1192b44e0cad7d34a2770f2744fcf13c7d
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-01-26 22:20:43 +0100

    feat(image upload): generate thumbnail

---
+++ api/utils/folder.js
@@ -0,0 +1,4 @@
+exports.verifyAndCreateFolder = (path) => {
+
+
+}
--- api/utils/images.js
+++ api/utils/images.js
@@ -1,14 +1,24 @@
 const fs = require('fs')
+const sharp = require('sharp');
 
 /**
+ * Uploads an image to a folder & generate a thumbnail
  * 
  * @param {Object} image - Image obtained from the multipart/form-data
  * @param {String} path - Path we want to upload the image to
  */
 exports.uploadImage = (image, path) => {
+  //TODO: verify path folder exists
   const uploadPath = path + image.originalname
+  
+  return new Promise(async (resolve, reject) => {
+    //Resize the image to a certain width
+    try {
+      await resizeImage(image, `${__dirname}/..${path}thumbnails/`)
+    }catch(e){
+      return reject(e)
+    }
 
-  return new Promise((resolve, reject) => {
     fs.writeFile(`${__dirname}/..${uploadPath}`, image.buffer, err => {
       if(err){
         return reject({
@@ -18,6 +28,28 @@ exports.uploadImage = (image, path) => {
       }
 
       resolve()
+    })
+  })
+}
+
+/**
+ * @param {Object} image - Image obtained from the multipart/form-data
+ * @param {String} path - Folder's path we want to upload the image to
+ */
+resizeImage = (image, path, width=350) => {
+  //TODO: verify path folder exists
+  return new Promise((resolve, reject) => {
+    sharp(image.buffer)
+    .resize(width)
+    .toFile(path + image.originalname, (err, info) => {
+      if(err){
+        return reject({
+          success: false,
+          message: `Une erreur est survenue lors du redimensionnement de l'image: ${err}`
+        })
+      }
+
+      return resolve()
     })
   })
 }

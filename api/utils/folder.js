410d70ff06e267bff566f1f61200bbb4ec5d56ae
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-01-27 12:59:39 +0100

    feat(image upload): verify if the folder given exists and create it if it doesn't

--- api/utils/folder.js
+++ api/utils/folder.js
@@ -1,4 +1,27 @@
+const fs = require('fs')
+
+/**
+ * Verify the existence of a folder, if it doesn't exist, it tries to create it
+ * 
+ * @param {String} path - Path to a folder 
+ * 
+ * @return {Promise} - Returns a promise that will be rejected if an error occured while creating the folder
+ */
 exports.verifyAndCreateFolder = (path) => {
+  return new Promise((resolve, reject) => (
+    fs.access(path, err => {
+      if(err){
+        return fs.mkdir(path, { recursive: true }, err => {
+          if(err) return reject({
+            success: false,
+            message: `Une erreur est survenue lors de la création du dossier: ${err}`
+          })
 
+          return resolve()
+        })
+      }
 
+      return resolve()
+    }) 
+  ))
 }
--- api/utils/images.js
+++ api/utils/images.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
 const sharp = require('sharp');
+const { verifyAndCreateFolder } = require('./folder');
 
 /**
- * Uploads an image to a folder & generate a thumbnail
+ * Uploads an image to a folder & generates a thumbnail
  * 
  * @param {Object} image - Image obtained from the multipart/form-data
  * @param {String} path - Path we want to upload the image to
  */
 exports.uploadImage = (image, path) => {
-  //TODO: verify path folder exists
   const uploadPath = path + image.originalname
   
   return new Promise(async (resolve, reject) => {
@@ -37,8 +37,14 @@ exports.uploadImage = (image, path) => {
  * @param {String} path - Folder's path we want to upload the image to
  */
 resizeImage = (image, path, width=350) => {
-  //TODO: verify path folder exists
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    //Verify that the folder given by path exists, if not it tries to create it
+    try {
+      await verifyAndCreateFolder(path)
+    }catch(e){
+      return reject(e) 
+    }
+
     sharp(image.buffer)
     .resize(width)
     .toFile(path + image.originalname, (err, info) => {

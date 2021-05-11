44c7005858199e254f6ab30a9d028b23a3c62063
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-05-11 13:59:50 +0200

    refactor(Sitemap): Use workflow to schedule sitemap regeneration

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,9 @@
-import { useEffect } from "react";
-import Header from "../components/Header/Header";
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
 import { FlashProvider } from "../context/Flash";
 import { LoggedProvider } from "../context/Logged";
-import sitemap from "../sitemap";
-import "../styles/index.scss"
+import "../styles/index.scss";
 
-function App({Component, pageProps}) {
+function App({ Component, pageProps }) {
   return (
     <LoggedProvider>
       <FlashProvider>
@@ -18,13 +15,10 @@ function App({Component, pageProps}) {
   );
 }
 
-export async function getStaticProps(){
-  sitemap()
-  window.setInterval(sitemap, 1000 * 60 * 60 * 24)
-
+export async function getStaticProps() {
   return {
-    props: {}
-  }
+    props: {},
+  };
 }
 
 export default App;

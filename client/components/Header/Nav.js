075c13ee9fd4b7cd451c88518efc476c84d3d0cd
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-06-12 15:36:01 +0200

    feat(Client): Add videos page

--- client/components/Header/Nav.js
+++ client/components/Header/Nav.js
@@ -1,48 +1,65 @@
-import Link from 'next/link'
-import { useContext } from 'react'
-import { FlashContext } from '../../context/Flash'
-import { LoggedContext } from '../../context/Logged'
+import Link from "next/link";
+import { useContext } from "react";
+import { FlashContext } from "../../context/Flash";
+import { LoggedContext } from "../../context/Logged";
 
-const Nav = ({nav, setNav}) => {
-  const [logged, setLogged] = useContext(LoggedContext)
-  const [, setFlash] = useContext(FlashContext)
+const Nav = ({ nav, setNav }) => {
+  const [logged, setLogged] = useContext(LoggedContext);
+  const [, setFlash] = useContext(FlashContext);
 
   const handleClick = async () => {
-    const data = await fetch('/api/auth/logout')
-    const json = await data.json()
+    const data = await fetch("/api/auth/logout");
+    const json = await data.json();
 
-    setNav(false)
-    setLogged({logged: false, data: ''})
-    setFlash({active: true, success: json.success, message: json.message})
-  }
+    setNav(false);
+    setLogged({ logged: false, data: "" });
+    setFlash({ active: true, success: json.success, message: json.message });
+  };
 
   const handleClickAdmin = () => {
-    window.location.pathname = '/admin'
-  }
-  
-  return <nav className={nav ? 'active' : ''}>
-    <ul>
-      <li><Link href="/">Accueil</Link></li>
-      <li><Link href="/galerie">galerie</Link></li>
-      <li><Link href="/carte">Carte</Link></li>
-      {
-        logged.logged
-        ?<>
-          <li><Link href="/profil">Mon profil</Link></li>
-          <li><button onClick={handleClick}>Se déconnecter</button></li>
-          {
-            logged.data.role === 'admin'
-            ?<li><button onClick={handleClickAdmin}>Admin</button></li>
-            :<></>
-          }
-        </>
-        :<>
-          {/* <li><Link href="/connexion">Se connecter</Link></li>
-          <li><Link href="/inscription">S'inscrire</Link></li> */}
-        </>
-      }
-    </ul>
-  </nav>
-}
+    window.location.pathname = "/admin";
+  };
 
-export default Nav
+  return (
+    <nav className={nav ? "active" : ""}>
+      <ul>
+        <li>
+          <Link href="/">Accueil</Link>
+        </li>
+        <li>
+          <Link href="/galerie">galerie</Link>
+        </li>
+        <li>
+          <Link href="/carte">Carte</Link>
+        </li>
+        <li>
+          <Link href="/videos">Vidéos</Link>
+        </li>
+        {logged.logged ? (
+          <>
+            <li>
+              <Link href="/profil">Mon profil</Link>
+            </li>
+            <li>
+              <button onClick={handleClick}>Se déconnecter</button>
+            </li>
+            {logged.data.role === "admin" ? (
+              <li>
+                <button onClick={handleClickAdmin}>Admin</button>
+              </li>
+            ) : (
+              <></>
+            )}
+          </>
+        ) : (
+          <>
+            {/* <li><Link href="/connexion">Se connecter</Link></li>
+          <li><Link href="/inscription">S'inscrire</Link></li> */}
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ client/pages/videos.js
@@ -0,0 +1,36 @@
+import YouTube from "react-youtube";
+
+const Videos = ({ videos }) => {
+  console.log(videos);
+
+  return (
+    <section className="videos">
+      <h1>Les vidéos youtube</h1>
+      <div>
+        {videos.items.map((video) => {
+          return <YouTube key={video.id.videoId} videoId={video.id.videoId} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export const getStaticProps = async () => {
+  const CHANNEL_ID = "UCzrtCQAmjlPvjm9x7hnluJw";
+
+  const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+  const YOTUBE_API_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&channelType=any&maxResults=25&order=date&type=video&videoCaption=videoCaptionUnspecified&videoDefinition=any&key=${YOUTUBE_API_KEY}`;
+
+  const videos = await fetch(YOTUBE_API_URL)
+    .then((res) => res.json())
+    .catch();
+
+  return {
+    props: {
+      videos,
+    },
+    revalidate: 60 * 15,
+  };
+};
+
+export default Videos;
